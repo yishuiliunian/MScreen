bcc2d66d9730b6fda5b50d6951edd12f1d1521d7
yishuiliunian <yishuiliunian@gmail.com>
2024-08-20 10:51:39 +0800

    feat: finally, I build Kingfisher successfully.
    Just update the version of XCode to 15.4. And the version of swift will be updated too. On the latest version of swift, it allow macros.

--- Products/MScreen/Sources/BazelApp.swift
+++ Products/MScreen/Sources/BazelApp.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 import Swinject
+import Kingfisher
 
 struct ContentView: View {
     var body: some View {
         VStack {
+            KFImage(URL(string:"https://p0.itc.cn/images01/20220125/f9e51d106a97408296b05784e3040d1c.jpeg"))
             Image(systemName: "globe")
                             .imageScale(.large)
                             .foregroundStyle(.tint)
